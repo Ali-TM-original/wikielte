57b7ebcb78cb9cea8d86b30b2a219756b80111eb
Shahzaib Ali <shahzaibaliofficial0@gmail.com>
2024-12-09 17:22:19 +0100

    Added Gemini

--- cogs/ai.py
+++ cogs/ai.py
@@ -3,12 +3,14 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from wikie import WikiElteBot
-from openai import OpenAI
+import google.generativeai as genai
 
 
 class AiCog(commands.Cog):
     def __init__(self, bot: WikiElteBot):
         self.bot: WikiElteBot = bot
+        self.model = genai.GenerativeModel("gemini-pro")
+        genai.configure(api_key=os.getenv("AI_API_KEY"))
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -17,20 +19,10 @@ class AiCog(commands.Cog):
     @app_commands.command(name="prompt", description="Process a prompt with OpenAI")
     async def prompt(self, interaction: discord.Interaction, prompt: str):
         try:
-            client = OpenAI()
-            completion = client.chat.completions.create(
-                model="gpt-3.5-turbo-instruct",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {
-                        "role": "user",
-                        "content": prompt
-                    }
-                ]
-            )
-            print(completion)
-            response_text = completion.choices[0].message
-            await interaction.response.send_message(response_text)
+            res = self.model.generate_content(prompt)
+            if not res.text or not res or res.text == '':
+                raise Exception
+            await interaction.response.send_message(res.text)
         except Exception as e:
             # Handle errors and send a friendly message
             await interaction.response.send_message(f"An error occurred: {str(e)}")
